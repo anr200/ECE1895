658bbdf8207212f9919bfe567f223601e3b8ac69
anr200 <anr200@pitt.edu>
2021-03-29 15:33:07 -0400

    Hello world file added

---
+++ Hello_world.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(7, OUTPUT);    // sets the digital pin 13 as output
+  pinMode(8, INPUT);
+}
+
+void loop() {
+  if(digitalRead(8) == LOW){
+    digitalWrite(7, HIGH); // sets the digital pin 13 on
+    delay(50);            // waits for a second
+    digitalWrite(7, LOW);  // sets the digital pin 13 off
+    delay(50); // waits for a second
+  }
+  else
+    digitalWrite(13, LOW);  // sets the digital pin 13 off
+}
